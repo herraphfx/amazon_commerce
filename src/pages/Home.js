9d3edd45ae7a460b6996332fa72f8416e4272b2a
herraphfx <herraph@gmail.com>
2022-07-14 21:44:37 +0400

    some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,14 +11,51 @@ function Home() {
        <img 
        className='home__image'
        src='https://m.media-amazon.com/images/I/81YSWfTFDbL._SX3000_.jpg' alt='home' />
-
-       <Product 
+    <div className='home__row'>
+    <Product 
        id='12345'
         title='The Lean Startup'
         price={11.96}
         rating={3}
         image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
        />
+        <Product 
+       id='12345'
+        title='The Lean Startup'
+        price={11.96}
+        rating={3}
+        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+       />
+        <Product 
+       id='12345'
+        title='The Lean Startup'
+        price={11.96}
+        rating={3}
+        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+       />
+         <Product 
+       id='12345'
+        title='The Lean Startup'
+        price={11.96}
+        rating={3}
+        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+       />
+        <Product 
+       id='12345'
+        title='The Lean Startup'
+        price={11.96}
+        rating={3}
+        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+       />
+        <Product 
+       id='12345'
+        title='The Lean Startup'
+        price={11.96}
+        rating={3}
+        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+       />
+    </div>
+      
     </div>
     </>
    
