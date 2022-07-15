1ae9df555b21b2ad418ef8c58d287fa097c7abac
herraphfx <herraph@gmail.com>
2022-07-15 21:42:21 +0400

    some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,38 +21,41 @@ function Home() {
        />
         <Product 
        id='12345'
-        title='The Lean Startup'
-        price={11.96}
+        title='AHOUDY Double Din Car Stereo Voice Control'
+        price={118.60}
         rating={3}
-        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+        image='https://m.media-amazon.com/images/I/717QUFs9qLL._AC_SX679_.jpg'
        />
         <Product 
        id='12345'
-        title='The Lean Startup'
-        price={11.96}
+        title='SAMSUNG Galaxy Watch 4'
+        price={339.00}
         rating={3}
-        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+        image='https://m.media-amazon.com/images/I/61im3OsaksL._AC_SX679_.jpg'
        />
          <Product 
        id='12345'
-        title='The Lean Startup'
-        price={11.96}
+        title='SINGER | 4423 Heavy Duty Sewing Machine'
+        price={129.50}
         rating={3}
-        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+        image='https://m.media-amazon.com/images/I/51JFZ7U25-L._AC_SX679_.jpg'
+       />
+   
+    </div>
+    <div className='home__row'>
+    <Product 
+       id='12345'
+        title='Net10 Alcatel TCL A3, 32GB'
+        price={39.40}
+        rating={3}
+        image='https://m.media-amazon.com/images/I/61-fB+GkifL._AC_SX679_.jpg'
        />
         <Product 
        id='12345'
-        title='The Lean Startup'
+        title='LINGVIDO Leather Car Seat Covers'
         price={11.96}
         rating={3}
-        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-       />
-        <Product 
-       id='12345'
-        title='The Lean Startup'
-        price={11.96}
-        rating={3}
-        image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+        image='https://m.media-amazon.com/images/I/71KSpyrF4YL._AC_SX679_.jpg'
        />
     </div>
       
