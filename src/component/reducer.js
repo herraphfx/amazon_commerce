cb4a7ca45e6d265fa4a7ef0cc0bbb9dfc6220986
herraphfx <herraph@gmail.com>
2022-07-24 21:02:19 +0400

    some changes

--- src/component/reducer.js
+++ src/component/reducer.js
@@ -8,7 +8,8 @@ function reducer(state, action) {
     switch(action.type) {
         case 'ADD_TO_BASKET':
             //logic for adding to basket
-            return { ...state }
+            return { ...state,
+            basket: [...state.basket, action.item]}
             break;
         case 'REMOVE_FROM_BASKET':
             //Logic for removing
