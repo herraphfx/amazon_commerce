c072df8d522a02c0f4d9bcaa03219ed3916bbf45
herraphfx <herraph@gmail.com>
2022-07-29 21:13:55 +0400

    some changes

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../component/Header'
 import { useStateValue } from '../component/StateProvider'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
   const [{basket}] = useStateValue();
@@ -13,11 +14,21 @@ function Checkout() {
      {basket?.length === 0 ? (
       <div>
         <h2>Your Shopping Basket is Empty</h2>
+        <p>You have no items in your basket. To buy one or more items, click "Add to Basket" next to the item</p>
       </div>
      ) : (
       <div>
-        <h2>Your Basket Empty</h2>
-        <p>You have no items in your basket. To buy one or more items, click "Add to Basket" next to the item</p>
+        <h2 className='checkout__title'>Your Shopping Basket</h2>
+        {/* List of all products */}
+        {basket.map(item => (
+          <CheckoutProduct
+          item={item.id}
+          title={item.title}
+          image={item.image}
+          price={item.price}
+          rating={item.price}
+          />
+        ))}
       </div>
      )}
     </div>
