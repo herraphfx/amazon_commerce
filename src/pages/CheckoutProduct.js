100c529926be6dea8e5c24cf19abad1dfa9fd8c3
herraphfx <herraph@gmail.com>
2022-08-09 16:43:48 +0400

    some changes

--- src/pages/CheckoutProduct.js
+++ src/pages/CheckoutProduct.js
@@ -7,7 +7,7 @@ function CheckoutProduct({id, title, image, price, rating}) {
         <div className='checkoutProduct__info'>
             <p className='checkoutProduct__title'>
                 {title}
-                console.log(title);
+               
             </p>
 
             <p className='checkoutProduct__price'>
