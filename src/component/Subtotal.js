d848e394bb5b7abd71d8d707d4522e486195e634
herraphfx <herraph@gmail.com>
2022-12-24 19:50:43 +0400

    changes

--- src/component/Subtotal.js
+++ src/component/Subtotal.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format';
 import { SportsBasketball } from '@mui/icons-material';
+import { getBasketTotal } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+  const [{basket}, dispatch] = useStateValue();
   return (
     <div className='subtotal'>
       <CurrencyFormat
@@ -20,7 +23,7 @@ function Subtotal() {
         </>
       )}
       decimalScale={2}
-      value={0}
+      value={getBasketTotal(basket)}
       displayType={"text"}
       thousandSeperator={true}
       prefix={"$"}
--- src/component/reducer.js
+++ src/component/reducer.js
@@ -18,6 +18,9 @@ export const initialState = {
     user: null
 };
 
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0);
+
 function reducer(state, action) {
     console.log(action);
     switch(action.type) {
